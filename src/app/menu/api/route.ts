152da8cefb1fbf640880c1a10eceb034f7c4ab3e
Musa Mutetwi <musasoftlabx@gmail.com>
2025-03-12 00:38:34 +0300

    Dev: Updates

--- src/app/menu/api/route.ts
+++ src/app/menu/api/route.ts
@@ -1,21 +1,11 @@
 import got from "got";
 
-//process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
-
 export async function GET(
   request: Request,
   { params }: { params: { id: string } }
 ) {
-  const res = await got("https://jsonplaceholder.typicode.com/todos", {
-    https: { rejectUnauthorized: false },
-  }).json();
-  // const users = await got("https://dealerportal.safaricom.co.ke/api/users", {
-  //   https: { rejectUnauthorized: false },
-  // }).json();
-
-  //console.log(res);
+  const res = await got("https://jsonplaceholder.typicode.com/todos/1").json();
   return Response.json(res);
-  //return Response.json(users);
 }
 
 export async function POST(request: Request) {
